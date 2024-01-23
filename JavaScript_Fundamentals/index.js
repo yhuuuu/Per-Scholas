3c2d983546184feca6940253abaa2b5be50f2843
Yuting Hu <yuting.hu.careers@gmail.com>
2024-01-22 18:18:39 -0800

    Functionfor calcaulate the weighted average score

--- JavaScript_Fundamentals/index.js
+++ JavaScript_Fundamentals/index.js
@@ -85,69 +85,96 @@ const LearnerSubmissions = [
 console.log("-------------")
 
 
+// Calcaulate the weighted average score
+function calWeightedAvg(assignments, submissions) {
+    let totalWeightedScore = 0;
+    let totalWeightAverage = 0;
+    let testScoreEarned =0;
+    let ScoreThatCanBeEarnedEachAssignment =0
+    let totalScoreEarned  =0
+    let totalScore =0
 
-let totalNumOfAssignment = AssignmentGroup.assignments.length
-let totoalNumOfStudent = LearnerSubmissions.length
+    //Loop for each assignment info
+    for (const assignment of assignments.assignments) {
+        //only work on submitted assignment id that are existed in the assignmentInfo
+        const submission = submissions.find(sub => sub.assignment_id === assignment.id)
 
-
-// Calculate Score
-function calculateScore(assignmentScore, assignmentTotalScore) {
-    return (assignmentScore / assignmentTotalScore)
-}
-// // Calculate each assignment sore 
-// function eachAssignmentSore() {
-//     let percentageScores = [];
-
-//     for (currentAssignment = 0; currentAssignment < totalNumOfAssignment; currentAssignment++) {
-
-//         let assignmentScore = LearnerSubmissions[currentAssignment].submission['score']
-//         let assignmentTotalScore = AssignmentGroup.assignments[currentAssignment].points_possible
-//         let percentage = (assignmentScore / assignmentTotalScore)
-
-//         percentageScores.push(percentage)
-
-//     }
-//     return percentageScores;
-// }
-// console.log(eachAssignmentSore())
-
-
-
-for (let currentStudent = 0; currentStudent < totoalNumOfStudent; currentStudent++) {
-
-    for (currentAssignment = 0; currentAssignment < totalNumOfAssignment; currentAssignment++) {
-        let assignmentScore = LearnerSubmissions[currentAssignment].submission['score']
-        let assignmentTotalScore = AssignmentGroup.assignments[currentAssignment].points_possible
-
-        let percentage = calculateScore(assignmentScore, assignmentTotalScore);
-        console.log(`Student ID ${LearnerSubmissions[currentStudent].learner_id}, Assignment ID ${AssignmentGroup.assignments[currentAssignment].id}, Percentage Score: ${percentage}`);
-
+        // if assigment id is exised and the assignment is not past due
+        if (submission && new Date(assignment.due_at) <= new Date) {
+            //const percentage = submission.submission.score / assignment.points_possible
+            testScoreEarned = submission.submission.score 
+            ScoreThatCanBeEarnedEachAssignment = assignment.points_possible
+            
+            totalScoreEarned += testScoreEarned
+            totalScore += ScoreThatCanBeEarnedEachAssignment
+            console.log(totalScoreEarned)
+            console.log(totalScore)
+            totalWeightAverage = totalScoreEarned/totalScore
+        }
     }
-
+    console.log(totalWeightAverage)
+    return totalWeightAverage 
 }
+calWeightedAvg(AssignmentGroup, LearnerSubmissions)
+
+console.log("######")
+// function calculateScore(assignmentScore, assignmentTotalScore) {
+//     return (assignmentScore / assignmentTotalScore) * 100;
+// }
+
+// function calEachAssignmentSore(submissions, currentStudent, ag) {
+//     let percentages = [];
+//     for (let currentAssignment = 0; currentAssignment < ag.assignments.length; currentAssignment++) {
+
+//         let assignmentScore = submissions[currentStudent].submission.score;
+//         let assignmentTotalScore = ag.assignments[currentAssignment].points_possible;
+
+//         let percentage = calculateScore(assignmentScore, assignmentTotalScore);
+//         percentages.push(percentage);
+//     }
+//     return percentages
+// }
 
 
 
 function getLearnerData(course, ag, submissions) {
-    // here, we would process this data to achieve the desired result.
-    // const result = [
-    //     {
-    //         id: 125,
-    //         avg: 0.985, // (47 + 150) / (50 + 150)
-    //         1: 0.94, // 47 / 50
-    //         2: 1.0 // 150 / 150
-    //     },
-    //     {
-    //         id: 132,
-    //         avg: 0.82, // (39 + 125) / (50 + 150)
-    //         1: 0.78, // 39 / 50
-    //         2: 0.833 // late: (140 - 15) / 150
-    //     }
-    // ];
+    let result = [];
 
+    // for (let currentStudent = 0; currentStudent < submissions.length; currentStudent++) {
+    //     let percentages = calEachAssignmentSore(submissions,currentStudent,ag)
+
+    //      for (let currentAssignment = 0; currentAssignment < ag.assignments.length; currentAssignment++) {
+    //         result.push({
+    //             learner_id: submissions[currentStudent].learner_id,
+    //             assignment_id: ag.assignments[currentAssignment].id,
+    //             percentage_score: percentages[currentAssignment].toFixed(0)
+    //         });
+    //     }
+
+    // }
     // return result;
 }
 
-// const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
+const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
 
-// console.log(result);
+console.log(result);
+
+
+
+// here, we would process this data to achieve the desired result.
+// const result = [
+//     {
+//         id: 125,
+//         avg: 0.985, // (47 + 150) / (50 + 150)
+//         1: 0.94, // 47 / 50
+//         2: 1.0 // 150 / 150
+//     },
+//     {
+//         id: 132,
+//         avg: 0.82, // (39 + 125) / (50 + 150)
+//         1: 0.78, // 39 / 50
+//         2: 0.833 // late: (140 - 15) / 150
+//     }
+// ];
+
+// return result;
