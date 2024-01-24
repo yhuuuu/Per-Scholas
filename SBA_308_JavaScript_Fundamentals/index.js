483405741357c34fcbb73aa3d8c75e1eda13dc2b
Yuting Hu <yuting.hu.careers@gmail.com>
2024-01-23 22:45:47 -0800

    Placed loops inside getLearnerData Fn

--- SBA_308_JavaScript_Fundamentals/index.js
+++ SBA_308_JavaScript_Fundamentals/index.js
@@ -76,24 +76,36 @@ const LearnerSubmissions = [
     }
 ];
 
-// Find out how many student in the data
-let studentsArray = [];
-// Loop for student submission information
-for (let submission of LearnerSubmissions) {
 
-    // To find an existing student in studentsArray whose id matches submission.learner_id. 
-    
-    let existingStudent = studentsArray.find(stu => stu.id === submission.learner_id)
 
-    // If the student is not exist, it will add the student
-    if (!existingStudent) {
-        studentsArray.push({ id: submission.learner_id });
+
+
+
+//let studentsArray = [];
+
+function getLearnerData(course, ag, submissions) {
+
+    //here, we would process this data to achieve the desired result.
+    const result = []
+    // Find out how many student in the data
+    let studentsArray = [];
+    // Loop for student submission information
+
+    for (let submission of submissions) {
+
+        // To find an existing student in studentsArray whose id matches submission.learner_id. 
+
+        let existingStudent = studentsArray.find(stu => stu.id === submission.learner_id)
+
+        // If the student is not exist, it will add the student
+        if (!existingStudent) {
+            studentsArray.push({ id: submission.learner_id });
+        }
+
     }
-
-}
-console.log("Student sumbitted the assignment:", studentsArray);
-
-let studentsGrades = {}
+  //  console.log("Student sumbitted the assignment:", studentsArray);
+  
+    let studentsGrades = {}
 // Loop for each submission assignment 
 
 
@@ -109,27 +121,7 @@ for (i = 0; i < studentsArray.length; i++) {
 
 console.log(studentsGrades)
 //console.log(JSON.stringify(studentsGrades, null, 2));
-
-
-
-
-function getLearnerData(course, ag, submissions) {
-
-    //here, we would process this data to achieve the desired result.
-    const result = [
-        {
-            id: 125,
-            avg: 0.985, // (47 + 150) / (50 + 150)
-            1: 0.94, // 47 / 50
-            2: 1.0 // 150 / 150
-        },
-        {
-            id: 132,
-            avg: 0.82, // (39 + 125) / (50 + 150)
-            1: 0.78, // 39 / 50
-            2: 0.833 // late: (140 - 15) / 150
-        }
-    ];
+console.log('-------')
     return result;
 }
 
@@ -138,3 +130,16 @@ const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
 console.log(result);
 
 
+
+console.log({
+    id: 125,
+    avg: 0.985, // (47 + 150) / (50 + 150)
+    1: 0.94, // 47 / 50
+    2: 1.0 // 150 / 150
+},
+    {
+        id: 132,
+        avg: 0.82, // (39 + 125) / (50 + 150)
+        1: 0.78, // 39 / 50
+        2: 0.833 // late: (140 - 15) / 150
+    })
