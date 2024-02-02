86d70489d5d8fa04ef85b152e8210afeb28a608b
Yuting Hu <yuting.hu.careers@gmail.com>
2024-02-02 01:50:31 -0800

    added css styles and event function

--- The Document Object Model/src/index.js
+++ The Document Object Model/src/index.js
@@ -1,7 +1,7 @@
 // First find the element from html
-const pname = document.getElementById("pname");
 const submitBtn = document.getElementById("submitBtn");
-const ptype = document.getElementById("ptype");
+const plantNameInput = document.getElementById("pname")
+const plantTypeSelect = document.getElementById("ptype")
 const pheight = document.getElementById("pheight");
 const pwidth = document.getElementById("pwidth");
 const conditionCheckBoxes = document.querySelectorAll("condition");
@@ -10,25 +10,28 @@ const preferenceCheckBox = document.querySelectorAll("problem")
 const postDate = document.getElementById("pdate")
 const swapListContainer = document.getElementsByClassName("swaplist-container")[0]
 
-
-
-//Create a table to store the input data
-
-
-
 // // Get the input value from the form after clicking the button
-// submitBtn.addEventListener('click', handleClick);
+submitBtn.addEventListener('click', handleClick);
 
-// function handleClick(event) {
-//     //prevent the default from submissoin behavior
-//     event.preventDefault();
 
-//     //Create new div
-//     const newDiv = document.createElement("div")
+function handleClick(event) {
+    //prevent the default from submissoin behavior
+    event.preventDefault();
 
-//     //new div = input value ==> <div> pname.value </div>
-//     newDiv.textContent = `Plant name :${pname.value} Plant Type:${ptype.value}`
+    //get  value from each input
+    const plantNameValue = plantNameInput.value
+    const plantTypeValue = plantTypeSelect.value
 
-//     //add value to the new div
-//     swapListContainer.appendChild(newDiv)
-// }
+    //insert a row into an HTML table when a add button is cliked  
+    const infoTableBody = document.getElementById('info-tbody')
+    const newRow = infoTableBody.insertRow(infoTableBody.rows.length);
+
+    // Insert cells and set their content
+    const cell1 = newRow.insertCell(0);
+    const cell2 = newRow.insertCell(1);
+
+    cell1.innerHTML = plantNameValue
+    cell2.innerHTML = plantTypeValue
+
+}
+
