628bcfd43ac194f31e39d39c7e0c285080f05d17
Yuting Hu <yuting.hu.careers@gmail.com>
2024-02-01 23:21:42 -0800

    modified html form structure

--- The Document Object Model/src/index.js
+++ The Document Object Model/src/index.js
@@ -1,6 +1,17 @@
 // First find the element from html
 const pname = document.getElementById("pname");
 const submitBtn = document.getElementById("submitBtn");
+const ptype = document.getElementById("ptype");
+const pheight = document.getElementById("pheight");
+const pwidth = document.getElementById("pwidth");
+const conditionCheckBoxes = document.querySelectorAll("condition")
+
+
+
+
+
+
+
 const swapListContainer = document.getElementsByClassName("swaplist-container")[0]
 
 
@@ -10,19 +21,13 @@ submitBtn.addEventListener('click', handleClick);
 function handleClick(event) {
     //prevent the default from submissoin behavior
     event.preventDefault();
-    //content
-    const pname_value = pname.value
-    
-    console.log(pname_value);
-    
-     //Create new div
-     const newDiv = document.createElement("div")
-     //new div = input value
-     newDiv.textContent = pname.value 
-     //console.log(newDiv);
-  
+
+    //Create new div
+    const newDiv = document.createElement("div")
+
+    //new div = input value ==> <div> pname.value </div>
+    newDiv.textContent = `Plant name :${pname.value} Plant Type:${ptype.value}`
+
     //add value to the new div
     swapListContainer.appendChild(newDiv)
-    
-
 }
