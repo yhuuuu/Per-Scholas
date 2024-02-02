d81b1467f0ce6adad016ab741b602f28067e68cc
Yuting Hu <yuting.hu.careers@gmail.com>
2024-02-02 14:23:32 -0800

    Created a Plant object to store each time button click add

--- The Document Object Model/src/index.js
+++ The Document Object Model/src/index.js
@@ -13,25 +13,62 @@ const swapListContainer = document.getElementsByClassName("swaplist-container")[
 // // Get the input value from the form after clicking the button
 submitBtn.addEventListener('click', handleClick);
 
+//Plant class
+class Plant {
+    constructor(name, type, height, width, conditon, problem, exchang_method, postDate) {
+        this.name = name
+        this.type = type
+        this.height = height
+        this.width = width
+        this.condition = conditon
+        this.problem = problem
+        this.exchang_method = exchang_method
+        this.postDate = postDate
+    }
+}
+
 
 function handleClick(event) {
-    //prevent the default from submissoin behavior
+    // Prevent the default form submission behavior
     event.preventDefault();
 
-    //get  value from each input
-    const plantNameValue = plantNameInput.value
-    const plantTypeValue = plantTypeSelect.value
+    // Get values from each input
+    const plantNameValue = plantNameInput.value;
+    const plantTypeValue = plantTypeSelect.value;
+    const plantHeightValue = pheight.value;
 
-    //insert a row into an HTML table when a add button is cliked  
-    const infoTableBody = document.getElementById('info-tbody')
+    // Create my first plant object
+    const plant = new Plant(plantNameValue, plantTypeValue, plantHeightValue);
+
+    // Log the plant object to check if values are captured correctly
+    console.log(plant);
+
+    // Create a new row and insert cells for each property of the plant object
     const newRow = infoTableBody.insertRow(infoTableBody.rows.length);
 
-    // Insert cells and set their content
-    const cell1 = newRow.insertCell(0);
-    const cell2 = newRow.insertCell(1);
-
-    cell1.innerHTML = plantNameValue
-    cell2.innerHTML = plantTypeValue
-
+    for (const property in plant) {
+        if (plant1.hasOwnProperty(property)) {
+            const cell = newRow.insertCell();
+            cell.innerHTML = plant[property];
+        }
+    }
 }
+
+//     //insert a row into an HTML table when a add button is cliked  
+//     const infoTableBody = document.getElementById('info-tbody')
+
+//     const newRow = infoTableBody.insertRow(infoTableBody.rows.length);
+
+//     // Insert cells and set their content
+//     const cell1 = newRow.insertCell(0);
+//     const cell2 = newRow.insertCell(1);
+//     const cell3 = newRow.insertCell(2)
+
+//     cell1.innerHTML = plantNameValue
+//     cell2.innerHTML = plantTypeValue
+//     cell3.innerHTML = plantHeightValue
+
+
+// }
+
 
