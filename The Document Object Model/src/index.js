7a23000ccdf3f70907f1b678f8032f989e56439a
Yuting Hu <yuting.hu.careers@gmail.com>
2024-02-01 22:15:28 -0800

    able to get the pname input value and store on a new div element

--- The Document Object Model/src/index.js
+++ The Document Object Model/src/index.js
@@ -2,19 +2,24 @@
 const pname = document.getElementById("pname");
 const submitBtn = document.getElementById("submitBtn");
 
-const swapList = document.getElementByClass("swaplist-container")
+const swapListContainer = document.getElementsByClassName("swaplist-container")
 // Get the input value from the form after clicking the button
 submitBtn.addEventListener('click', handleClick);
 
-function handleClick() {
-    //Create new div
-    const newDiv = document.createElement("div")
+function handleClick(event) {
+    //prevent the default from submissoin behavior
+    event.preventDefault();
     //content
     const pname_value = pname.value
     
+    console.log(pname_value);
     
+     //Create new div
+     const newDiv = document.createElement("div")
+     newDiv.textContent = pname.value 
+     console.log(newDiv);
+    //  newDiv.textContent = pname.value;
     // //add value to the new div
-    // newDiv.appendChild(pname_value)
-
+    // swapListContainer.appendChild(newDiv)
 
 }
