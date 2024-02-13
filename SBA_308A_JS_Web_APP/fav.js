54fad8ff6af738b7daff9bf37fa55be4813a66ea
Yuting Hu <yuting.hu.careers@gmail.com>
2024-02-12 22:21:22 -0800

    added a fav btn

---
+++ SBA_308A_JS_Web_APP/fav.js
@@ -0,0 +1,7 @@
+const test = document.getElementById('fav-container')
+
+const test2=document.createElement('h1')
+
+test2.textContent = 'HI'
+
+test.appendChild(test2)
--- SBA_308A_JS_Web_APP/index.js
+++ SBA_308A_JS_Web_APP/index.js
@@ -1,6 +1,7 @@
 
-const dogsSelectBox= document.getElementById('dog_breed_select');
+const dogsSelectBox = document.getElementById('dog_breed_select');
 const dogSearchBtn = document.getElementById('dog_breed_select_btn')
+const dogImgBox = document.getElementById('dogImg-container')
 
 const API_KEY = `live_6olo9RBewaIncUjmWKJFmPBas3EtGEQI3bU5nFZtdtXP0JuhZsN6f9PUXyq8sekg`
 
@@ -8,40 +9,72 @@ axios.defaults.baseURL = 'https://api.thedogapi.com/v1';
 axios.defaults.headers.common['x-api-key'] = API_KEY;
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
-
+/// Function to display all dog breeds in the select box
 async function displayDog() {
     //get dog list
-    const allDogsInfo = await axios.get(`/breeds`)
+    const allDogsInfo = await axios.get(`/breeds/`)
     const data = allDogsInfo.data;
-    //console.log(data);
 
-   //appent to the dogsSelectBox
-   data.forEach(breed => {
-    
-    const option = document.createElement('option')
-    
-    //let the value = breed_id
-    option.value = breed.id
-    option.text = breed.name
-   // console.log(option);
-   dogsSelectBox.appendChild(option)
-   });
+    //appent dog breeds to the select box
+    data.forEach(breed => {
+        const option = document.createElement('option')
+
+        //let the value = breed_id
+        option.value = breed.id
+        option.text = breed.name
+        // console.log(option);
+        dogsSelectBox.appendChild(option)
+    });
 
 
 }
 
-displayDog()
 
-dogSearchBtn.addEventListener('click',async()=>{
-    const breedType = dogsSelectBox.value
-    console.log('show img for',breedType);
-    // const dogImg = document.createElement('img')
-})
+// Function to display all dog breeds in the select box and set up event listener for button click
+async function setUpDogApp() {
 
-
-// async function fetchImg(){
-//     const imgUrl = await axios.get(`v1/images/:image_id${img_id}?`)
-
-// }
+    // display all dog breeds in the select ox
+    await displayDog();
 
 
+    // add event listener for button click to fetch images
+    dogSearchBtn.addEventListener('click', async(event) => {
+        event.preventDefault();
+        //fetch image for the select breed
+        await fetchImg(dogsSelectBox.value)
+    })
+
+}
+
+
+
+
+
+// Function to fetch images for the selected breed and display them
+async function fetchImg(dogsBreed) {
+
+    dogImgBox.innerHTML = ''; // clear existing imges
+
+    //ferch imges for the selected dog breed by breed id
+    const dogImgList = await axios.get(`images/search?breed_ids=${dogsBreed}`)
+
+    //display images of the selected dog breed
+    dogImgList.data.forEach(element => {
+
+        const dogImg = document.createElement('img')
+        dogImg.src = element.url
+        dogImgBox.appendChild(dogImg)
+        
+        const favBtn = document.createElement('button')
+        favBtn.textContent = 'add to fav!'
+        dogImgBox.appendChild(favBtn)
+        favBtn.classList.add('addToFav'); // Add CSS class to the button
+
+
+    });
+
+
+}
+
+// Call the setupDogApp function to initialize the application
+setUpDogApp()
