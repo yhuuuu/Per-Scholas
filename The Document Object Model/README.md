09179ac12bf53d4d97ca0867ad4e7d4e345a8595
Yuting Hu <99712824+yhuuuu@users.noreply.github.com>
2024-02-15 15:35:06 -0800

    Dom sba (#7)
    * folder initiate
    
    * created basic html structure
    
    * get input pname.value by add button
    
    * able to get the pname input value and store on a new div element
    
    * able to add new input div to the swapList contianer
    
    * modified html form structure
    
    * Create a table to store the input data
    
    * added css styles and event function
    
    * Created a Plant object to store each time button click add
    
    * able to return object but some value are undefind
    
    * add funtion that loops through checked checkbox and return as a array
    
    * finished problem selector section"
    
    * finished two descripton box
    
    * reset button
    
    * msg window
    
    * added pop up window
    
    * pop up windows tabs
    
    * pop window tab
    
    * finished pop up windows
    
    * added bootstrap
    
    * fix bug when select radio value "no"  return comments
    
    * bootstrap for pop up window
    
    * added close button
    
    * added readme and screenshots folder
    
    * updated readme

---
+++ The Document Object Model/README.md
@@ -0,0 +1,56 @@
+# The Document Object Model: The Plant Swap Website
+
+## Description
+
+This assessment evaluates comprehension of the Document Object Model (DOM) and ability to apply its features in a practical way. The purpose of this website is to provide a platform for users to submit information about plants that they wish to swap. The website also includes a pop-up feature that provides users with information to diagnose plant problems while they are filling out the submission form.
+
+This project includes HTML, CSS , JavaScript and Bootstrap (Bootstrap is automatically fetched at [index.html](https://github.com/yhuuuu/Per-Scholas/blob/35f84190b93a3dea8465d79d61a24cd57f7c8d58/The%20Document%20Object%20Model/index.html#L9C1-L13C41) and [problem.html](https://github.com/yhuuuu/Per-Scholas/blob/35f84190b93a3dea8465d79d61a24cd57f7c8d58/The%20Document%20Object%20Model/problem.html#L8C1-L12C42)) 
+
+
+
+
+
+## Features
+
+- **Easy to Submit:** Simple to entry the information and click submit button.
+
+- **Easy to Reset Form:** Reset button.
+
+- **Pop Up Plant Problem Window:** Easy to diagnose plant problems while filling out the submission form. Comment box only show up when user select "Yes".
+
+
+### How to Use
+
+Follow these steps to set up SwapPlant locally:
+
+```bash
+# Clone the greenSprout repository
+git clone git@github.com:yhuuuu/Per-Scholas/tree/DOM_SBA
+
+
+# Go to GreenSprout folder
+cd Per-Scholas/The Document Object Model/
+
+# Open index.html file
+open index.html
+
+```
+
+## Screenshots
+
+### Desktop View
+![Desktop Screenshot](./screenshots/demo.png)
+
+
+## Authors
+
+Yuting Hu  
+
+
+
+## Acknowledgments
+
+Inspiration, code snippets, etc.
+* [Pop up window - Top right](https://github.com/MaxArt2501/share-this/issues/17)
+
+
---
+++ The Document Object Model/src/index.js
@@ -0,0 +1,179 @@
+// First find the element from html
+const addBtn = document.getElementById("addBtn");
+const resetBtn = document.getElementById("resetBtn")
+const plantNameInput = document.querySelector(".form-container>form>div:first-child input");
+const plantTypeSelect = document.getElementById("ptype");
+const pheight = document.getElementById("pheight");
+const pwidth = document.getElementById("pwidth");
+const conditionCheckBoxes = document.querySelectorAll(".condition");
+const pproblem = document.querySelectorAll(".problem");
+const preferenceCheckBoxs = document.querySelectorAll(".option");
+const postDate = document.getElementById("pdate");
+
+const swapListContainer = document.getElementsByClassName("swaplist-container")[0];
+const additonalBox = document.getElementById("additionalContent")
+const problemDesBox = document.getElementById('problem_des')
+const pdesTextarea = document.getElementById("pdes")
+
+// // Get the input value from the form after clicking the button
+addBtn.addEventListener('click', handleAddButton);
+resetBtn.addEventListener('click', () => handleRestButton(document.getElementById('swap-from'), plantsArray));
+
+//Create a Plant class
+class Plant {
+    constructor(name, type, height, width, conditon, problem, problem_des, exchange_method, postDate, description) {
+        this.name = name
+        this.type = type
+        this.height = height
+        this.width = width
+        this.condition = conditon
+        this.problem = problem
+        this.problem_des = problem_des
+        this.exchange_method = exchange_method
+        this.postDate = postDate
+        this.description = description
+    }
+}
+// Array to store plant objects
+const plantsArray = []
+
+function handleAddButton(event) {
+    // Prevent the default form submission behavior
+    event.preventDefault();
+
+    // Get values from each input
+    const plantNameValue = plantNameInput.value;
+    const plantTypeValue = plantTypeSelect.value;
+    const plantHeightValue = pheight.value;
+    const plantWidthValue = pwidth.value;
+
+    const plantCondition = getPlantCondition(conditionCheckBoxes)
+    const plantProblem = getSelectedRadioValue(pproblem)
+    const plantProblemDescription = problemDesBox.value
+    const exchange_prefrence = getExhangePreference(preferenceCheckBoxs)
+
+    const plantPostDayValue = postDate.value
+    const plantDescription = pdesTextarea.value
+
+    // Create the plant object
+    const plant = new Plant(plantNameValue, plantTypeValue, plantHeightValue, plantWidthValue, plantCondition, plantProblem, plantProblemDescription, exchange_prefrence, plantPostDayValue, plantDescription);
+
+    // Add the plant object to the array
+    plantsArray.push(plant);
+
+    // Log the plant object to check if values are captured correctly
+    console.log(plantNameValue);
+
+
+    /**
+     * Creating a new row (<tr>) and appending it to an HTML table. 
+     * infoTableBody: <tbody> element 
+     * infoTableBody.rows.length: retrieves the number of rows currently in the table body 
+     * infoTableBody.insertRow(infoTableBody.rows.length): creates a new table row (<tr>) and inserts it at the end of the table body. 
+     */
+    const newRow = infoTableBody.insertRow(infoTableBody.rows.length);
+
+    //iterate over the properties of plant object (plantNameValue, plantTypeValue, plantHeightValue...)
+    for (const property in plant) {
+        // checks if the property being iterated over is a direct property of the object itself
+        if (plant.hasOwnProperty(property)) {
+            //creates a new table cell (<td>) and assigns it to the variable cell
+            const newCell = document.createElement('td')
+            //const cell = newRow.insertCell();
+
+            //sets the content of the newly created cell to the value of the current property of the plant object being iterated over
+            //cell.innerHTML = plant[property];
+            newCell.innerHTML = plant[property]
+            // console.log(cell);
+            newRow.appendChild(newCell);
+
+        }
+    }
+}
+
+
+
+//Get all conditoins that is checked and store in a array
+function getPlantCondition(conditionCheckBoxes) {
+    const selectedConditions = []
+    for (const checkbox of conditionCheckBoxes) {
+        if (checkbox.checked) {
+            selectedConditions.push(checkbox.value)
+        }
+    }
+    return selectedConditions
+}
+
+//add event listener to all answer
+for (const radioButton of pproblem) {
+    radioButton.addEventListener('change', () => handleProblemChange(radioButton.value))
+}
+
+//Get selected answer for plant radio button is click
+function handleProblemChange(buttonValue) {
+    let selectedValue = buttonValue
+    // if yes -->box show, return value and box value
+    // if no --> box hid. return value only
+    additonalBox.style.display = (buttonValue === 'yes') ? 'block' : 'none';
+
+
+    return selectedValue
+}
+//Get selected plant problem anwser 
+function getSelectedRadioValue(radioButtons) {
+
+    for (const radioButton of radioButtons) {
+        if (radioButton.checked) {
+            if (radioButton.value === 'no') {
+                problemDesBox.value = ''
+            }
+            return radioButton.value;
+        }
+    }
+}
+
+//Get all exhange preference that is checked and store in a array
+function getExhangePreference(preferenceCheckBoxs) {
+    const selectedExchangePerference = []
+    for (const checkbox of preferenceCheckBoxs) {
+        if (checkbox.checked) {
+            selectedExchangePerference.push(checkbox.value)
+        }
+    }
+    return selectedExchangePerference
+}
+
+
+// Helper function for reset button event
+function handleRestButton(form, plants) {
+    // Reset form values
+    form.reset();
+
+    // Reset plant objects
+    plants.forEach(plant => {
+        for (const property in plant) {
+            if (plant.hasOwnProperty(property)) {
+                plant[property] = "";
+            }
+        }
+    });
+
+    // Reset additional box style
+    additonalBox.style.display = 'none';
+}
+
+
+//Create a message window
+// /https://github.com/MaxArt2501/share-this/issues/17
+function problemBtn() {
+    const width = 700;
+    const height = 500;
+    const left = screen.availLeft + (screen.availWidth - width) / 2;
+    const top = screen.availTop + (screen.availHeight - height) / 2;
+    window.open('./problem.html', 'mywin', `width=${width},height=${height},left=${left},top=${top}`);
+
+}
+
+
+
+
