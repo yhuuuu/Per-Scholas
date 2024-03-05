e6c3937da2768cef7a8e5326351cb4995fec9ce2
yhu <99712824+yhuuuu@users.noreply.github.com>
2024-03-05 00:45:03 -0800

    Lab 320 h.2.1 (#13)
    * pj init
    
    * assignment completed

---
+++ Lab/Lab_320.2.1/src/App.jsx
@@ -0,0 +1,25 @@
+function Header() {
+  return (<h1>Simple React Application</h1>);
+}
+
+function Content(props) {
+  return (<p style={{color: props.color}}>{props.text}</p>);
+}
+
+function Footer() {
+  return (<h1>Created by Me, of course.</h1>);
+}
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Content color="blue" text="This is my first React Application!" />
+      <Content color="red" text="Wish me luck..." />
+      <Content color="green" text="I think I've got it!" />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
